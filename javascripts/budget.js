45a49d9e0670cd8a9465f569c09fb0164408aea3
April Nichols <nicholsar@gmail.com>
2018-04-29 10:49:27 -0500

    progress bar added

--- javascripts/budget.js
+++ javascripts/budget.js
@@ -1,5 +1,3 @@
-// const data = require('./data');
-
 const compareToBudget = (selections) => {
   let budgetTotal = document.getElementById('budget-amt').value;
   budgetTotal = budgetTotal * 1;
@@ -10,22 +8,6 @@ const compareToBudget = (selections) => {
   const costDifference = budgetTotal - totalElementsCost;
   return costDifference;
 };
-
-// const printBudgetComparison = (amount) => {
-//   const selectionTotalDiv = document.getElementById('selection-total-div');
-//   selectionTotalDiv.innerHTML = budgetComparisonString(amount);
-// };
-
-// const budgetComparisonString = (amount) => {
-//   let budgetDifference = '';
-//   if (amount >= 0) {
-//     budgetDifference += `<h3 class="budget-difference green">$${amount}</h3>`;
-//   } else {
-//     budgetDifference += `<h3 class="budget-difference red">$${amount}</h3>`;
-//   };
-//   budgetDifference += `<h4>Remaining</h4>`;
-//   return budgetDifference;
-// };
 
 module.exports = {
   compareToBudget,
